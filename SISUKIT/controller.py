9c93a3f7a952c8c2f3acb530a307419105d4ecc5
aji wuryanto <aji.wuryanto@ui.ac.id>
2019-09-27 16:59:11 +0700

    menambahkan gitignore, ubah struktur project

--- SISUKIT/controller.py
+++ SISUKIT/controller.py
@@ -14,4 +14,14 @@ def list_surat_sakit_sekre():
 def detil_surat_sakit_sekre():
     return 'Detail dari Surat Sakit Mahasiswa {NPM+ID Surat}' 
     
-       
+@app.route('/mahasiswa/listSuratSakit')       
+def list_surat_sakit_mahasiswa():
+    return 'List Surat Sakit Mahasiswa'
+
+@app.route('/mahasiswa/detilSuratSakit')
+def detil_surat_sakit_mahasiswa():
+    return 'Detail dari surat sakit mahasiswa {ID}'
+
+@app.route('/mahasiswa/kirimSuratSakit')
+def kirim_surat_sakit():
+    return 'Kirim Surat Sakit Mahasiswa'    
