40d3c2bceb8f9c7553a6952fceca2d35705bc2c9
aji wuryanto <aji.wuryanto@ui.ac.id>
2019-11-22 08:41:23 +0000

    script to start

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-app="sisukit"
-docker build -t ${app} .
-docker run -d -p 56733:80 \
-  --name=${app}
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=SISUKIT
+export FLASK_ENV=development
+nohup flask run -h 0.0.0.0 -p 8080 &
